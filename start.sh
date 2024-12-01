7a1a8c0369e04dd61e650df6c28b8b5c8983d7c0
Thelastone <142857872+itskissu@users.noreply.github.com>
2024-12-01 21:42:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/UnknownBotz/AutoFilterWithStream.git /AutoFilterWithStream
+  git clone https://github.com/itskissu/AutoFilterWithStream_Upload /AutoFilterWithStream_Upload
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AutoFilterWithStream
 fi
-cd /AutoFilterWithStream
+cd /AutoFilterWithStream_Upload
 pip3 install -U -r requirements.txt
 echo "Starting AutoFilterWithStream...."
 python3 bot.py
